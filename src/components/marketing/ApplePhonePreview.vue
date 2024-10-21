4b6e041a8b687fbe6469c288463c55c4b28a595f
Muhammed Nabeel <m.nabeel@fifty2.ae>
2024-10-21 19:14:12 +0000

    Squashed commit of the following
    commit 6a6cfdf3 
    Author: Muhammad Nabeel <m.nabeel@fifty2.ae> 
    Date: Mon Oct 21 2024 18:45:40 GMT+0200 (Central European Summer Time) 
    
        added google/apple marketing pages with post message through pinia
    
    
    commit 022231cf 
    Author: Muhammad Nabeel <m.nabeel@fifty2.ae> 
    Date: Thu Oct 17 2024 09:22:52 GMT+0200 (Central European Summer Time) 
    
        added marketing google static page
    
    
    commit c4a9d582 
    Author: Muhammad Nabeel <m.nabeel@fifty2.ae> 
    Date: Wed Oct 16 2024 13:14:53 GMT+0200 (Central European Summer Time) 
    
        added google page with mobile preview
    
    
    commit b7867397 
    Author: Muhammad Nabeel <m.nabeel@fifty2.ae> 
    Date: Tue Oct 15 2024 19:25:55 GMT+0200 (Central European Summer Time) 
    
        added google marketing page

---
+++ src/components/marketing/ApplePhonePreview.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="w-100 pb-3">
+    <div class="d-flex align-center justify-center flex-column">
+      <div class="preview-container">
+        <div class="pb-3">
+          <div class="google-container">
+            <v-img src="/images/loyalty/iphoneLayout.svg" alt="" width="300px">
+              <div class="notification-container">
+                <div class="notification-window">
+                  <div class="company-icon"></div>
+                  <header class="notification-header">
+                    <div class="header-content">
+                      <span class="company-name"> • wally • now</span>
+                    </div>
+                  </header>
+                  <div class="content">{{ notificationData?.message || 'Default message' }}</div>
+                </div>
+              </div>
+            </v-img>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  notificationData: {
+    type: Object,
+    default: () => ({})
+  }
+});
+
+</script>
+
+<style>
+.notification-container {
+  position: relative;
+  top: 170px;
+  width: 260px;
+  cursor: auto;
+  margin: auto;
+}
+
+.notification-window {
+  display: grid;
+  grid-template-columns: 24px auto 24px;
+  grid-template-areas:
+    "icon head collapse"
+    "blank message message";
+  grid-template-rows: auto;
+  background-color: rgb(255, 255, 255);
+  box-shadow: rgba(24, 39, 75, 0.12) 0px 6px 12px -6px, rgba(24, 39, 75, 0.08) 0px 8px 24px -4px;
+  gap: 12px;
+  padding: 20px;
+  border-radius: 28px;
+}
+
+.notification-header {
+  grid-area: head;
+  display: grid;
+  gap: 4px 8px;
+  width: 100%;
+  height: auto;
+  font-size: 12px;
+  grid-template: auto / auto;
+}
+
+.header-content {
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  column-gap: 1px;
+}
+
+.company-name {
+  min-width: max-content;
+  white-space: nowrap;
+  color: #051b2c;
+}
+
+.content {
+  grid-area: message;
+  color: #051b2c;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 400;
+  letter-spacing: 0.1px;
+  margin: 0px;
+  overflow: hidden;
+  width: 100%;
+  text-overflow: ellipsis;
+  white-space: pre-wrap;
+  word-break: break-word;
+}
+
+.company-icon {
+  grid-area: icon;
+  display: flex;
+  background-image: url('/favicon.ico');
+  background-size: contain; 
+  background-repeat: no-repeat;
+  width: 44px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
