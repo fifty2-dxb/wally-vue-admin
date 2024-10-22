9af85f3ae090ce9a164c8acbe0b46f042ebee8a5
Oktay Curebal <oktay@freshapps.nl>
2024-10-22 11:15:46 +0200

    Added the prices and the materials

--- src/pages/pages/advertisement/materials/index.vue
+++ src/pages/pages/advertisement/materials/index.vue
@@ -3,23 +3,52 @@ import { count } from 'console';
 
 const  materials = ref([
   {
-    name: "Table Tent",
+    name: "Table tent (A5)",
     description: "Both sides are front. Can be placed on tables in a cafe, on a reception desk, and so on.",
-    price: 5.25,
-    imageUrl: "https://getpassfiles.fra1.digitaloceanspaces.com/getpass/promotional_materials/table-tent-branded_en@3x.png",
+    price: 18.36,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/tabletent.png",
     count: 1
   },
   {
-    name: "Table Tent 2",
-    description: "Table Tent Description",
-    price: 6.25,
-    imageUrl: "https://getpassfiles.fra1.digitaloceanspaces.com/getpass/promotional_materials/table-tent-branded_en@3x.png",
-    count: 2
+    name: "Table tent (A6)",
+    description: "Both sides are front. Can be placed on tables in a cafe, on a reception desk, and so on.",
+    price: 14.69,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/tabletent.png",
+    count: 1
+  },
+  {
+    name: "Table tent with stand (A5)",
+    description: "Both sides are front. Can be placed on tables in a cafe, on a reception desk, and so on.",
+    price: 36.73,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/tabletent-stand.png",
+    count: 1
+  },
+  {
+    name: "Table tent with stand (A6)",
+    description: "Both sides are front. Can be placed on tables in a cafe, on a reception desk, and so on.",
+    price: 29.38,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/tabletent-stand.png",
+    count: 1
+  },
+  {
+    name: "Flyer (A6)",
+    description: "On lightweight paper for a single use. You can invest in an account, invest on delivery, and so on.",
+    price: 2.20,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/wally-flyers.png",
+    count: 20
+  },
+  {
+    name: "Sticker (53 × 81 mm)",
+    description: "Strong adhesive, but without a trace after peeling off. Can be glued to the mirror, to the surface of the cash desk and so on.",
+    price: 1.36,
+    imageUrl: "https://wally-images-bucket-dev.s3.eu-central-1.amazonaws.com/stickers.png",
+    count: 54
   },
 ]);
 
 const calculateTotal = computed(() => {
-  return materials.value.reduce((acc, material) => acc + material.price * material.count, 0);
+  const total = materials.value.reduce((acc, material) => acc + material.price * material.count, 0);
+  return parseFloat(total.toFixed(2)); // Ensures the result is a number, not a string
 });
 
 </script>
