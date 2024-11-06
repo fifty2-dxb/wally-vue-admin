21d35791024f292ca9d72d57ba76596806cefdab
Muhammad Nabeel <m.nabeel@fifty2.ae>
2024-11-06 17:43:31 +0500

    fixed campaign scroll issue

--- src/components/campaign/CardDesign.vue
+++ src/components/campaign/CardDesign.vue
@@ -501,35 +501,8 @@ const value = computed({
 });
 
 onMounted(() => {
-  var previewContainer = document.getElementById("previewContainer"),
-    // -60 so it won't be jumpy
-    stop = previewContainer.offsetTop + 150,
-    docBody =
-      document.documentElement || document.body.parentNode || document.body,
-    hasOffset = window.pageYOffset !== undefined,
-    scrollTop;
-
-  window.onscroll = function (e) {
-    // cross-browser compatible scrollTop.
-    scrollTop = hasOffset ? window.pageYOffset : docBody.scrollTop;
-
-    // if user scrolls to 60px from the top of the left div
-    if (scrollTop >= stop) {
-      // stick the div
-      previewContainer.className = "stick";
-    } else {
-      // release the div
-      previewContainer.className = "";
-    }
-  }; // Check if this logs to the console
   updateStampImage();
 });
-
-const isOpen = ref([0]);
-const isOpenTwo = ref([0]);
-const isOpenThree = ref([0]);
-const selectedStampImage = ref(null);
-const selectedUnStampImage = ref(null);
 
 const stampImages = ref([
   "anchor",
